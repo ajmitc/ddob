a70aaaf84fc2e4d24ecc76fd38de580b96eeccb8
ajmitc <ajmitc@gmail.com>
2018-08-01 13:19:24 -0400

    Added methods to Util and improved NotificationView

--- src/main/java/ddob/util/Util.java
+++ src/main/java/ddob/util/Util.java
@@ -7,6 +7,56 @@ public class Util {
     public static Dimension getScreenSize() {
         return Toolkit.getDefaultToolkit().getScreenSize();
     }
+	
+	public static Dimension calculateStringDimension( Graphics graphics, Font font, String text ) {
+		// get metrics from the graphics
+		FontMetrics metrics = graphics.getFontMetrics( font );
+		// get the height of a line of text in this
+		// font and render context
+		int hgt = metrics.getHeight();
+		// get the advance of my text in this font
+		// and render context
+		int adv = metrics.stringWidth( text );
+		// calculate the size of a box to hold the text
+		return new Dimension( adv, hgt );
+	}
+	
+	public static Dimension calculateStringDimension( Graphics graphics, Font font, List<String> text, int lineSpacing ) {
+		// get metrics from the graphics
+		FontMetrics metrics = graphics.getFontMetrics( font );
+		// get the height of a line of text in this
+		// font and render context
+		int hgt = (metrics.getHeight() + lineSpacing) * text.size();
+		// get the advance of my text in this font
+		// and render context
+		int maxAdv = 0;
+		for( String s: text ) {
+			int adv = metrics.stringWidth( s );
+			if( adv > maxAdv ) {
+				maxAdv = adv;
+			}
+		}
+		// calculate the size of a box to hold the text
+		return new Dimension( maxAdv, hgt );
+	}
+	
+	public static int getFontHeight( Graphics g, Font f ) {
+		FontMetrics metrics = g.getFontMetrics( f );
+		return metrics.getHeight();
+	}
+	
+	public static List<String> breakString( String s, int max_length ) {
+		List<String> parts = new ArrayList<>();
+		while( s.length() > max_length ) {
+			String part = s.substring( 0, max_length );
+			parts.add( part );
+			s = s.substring( max_length );
+		}
+		if( s.length() > 0 ) {
+			parts.add( s );	
+		}
+		return parts;
+	}
 
     private Util() {
 
--- src/main/java/ddob/view/NotificationView.java
+++ src/main/java/ddob/view/NotificationView.java
@@ -14,9 +14,14 @@ import java.awt.event.MouseEvent;
 public abstract class NotificationView {
 	public static final Color NOTIFICATION_BG_COLOR = Color.BLACK;
 	public static final Color NOTIFICATION_FG_COLOR = Color.WHITE;
+	public static final int MAX_TEXT_LENGTH = 30;
+	public static final Font FONT = new Font( "Serif", Font.PLAIN, 16 );
+	
+	public List<NotificationPanel> _panels;
 	
     public NotificationView( Model model, View view ) {
         super( model, view );
+		_panels = new ArrayList<>();
     }
 
     /**
@@ -31,6 +36,31 @@ public abstract class NotificationView {
 			Notification notification = iter.next();
 			if( notification.shouldHide() ) {
 				iter.remove();
+				NotificationPanel panelToRemove = null;
+				for( NotificationPanel panel: _panels ) {
+					if( panel.getNotification() == notification ) {
+						panelToRemove = panel;
+						break;
+					}
+				}
+				if( panelToRemove != null ) {
+					_panels.remove( panelToRemove );
+				}
+			}
+			else {
+				// Check if panel exists
+				boolean found = false;
+				for( NotificationPanel panel: _panels ) {
+					if( panel.getNotification() == notification ) {
+						found = true;
+						break;
+					}
+				}
+				if( !found ) {
+					// Add panel
+					NotificationPanel panel = new NotificationPanel( notification );
+					_panels.add( panel );
+				}
 			}
 		}
 	}
@@ -38,11 +68,10 @@ public abstract class NotificationView {
 	@Override
     public void redraw( Graphics2D g, Dimension panelSize ) {
 		// Draw notifications on screen
-		int turnTrackHeight = _gamePanel.getTurnTrackView().getTurnTrackImage().getHeight( null );
+		int turnTrackHeight = _view.getGamePanel().getTurnTrackView().getTurnTrackImage().getHeight( null );
 		int x = panelSize.getWidth() - 100; // centered-ish
 		int y = turnTrackHeight + 3;
-		for( Notification notification: _view.getNotifications() ) {
-			NotificationPanel panel = new NotificationPanel( notification );
+		for( NotificationPanel panel: _panels ) {
 			panel.draw( g, x, y );
 			y += panel.getHeight();
 		}
@@ -50,18 +79,34 @@ public abstract class NotificationView {
 	
 	class NotificationPanel {
 		private Notification _notification;
+		private List<String> _content;
+		private int _width, _height, _lineHeight;
+		
 		public NotificationPanel( Notification notification ) {
 			_notification = notification;
+			_content = Util.breakString( _notification.getContent(), MAX_TEXT_LENGTH );
+			_width = -1;
+			_height = -1;
 		}
 		
 		public void draw( Graphics2D g, int x, int y ) {
-			String content = _notification.getContent();
-			int w = 0;  // Measure width
-			int h = 0;  // Measure height
+			if( _width < 0 || _height < 0 ) {
+				Dimension textSize = Util.calculateStringDimension( g, FONT, _content, 2 );
+				_width = textSize.width;
+				_height = textSize.height;
+				_lineHeight = Util.getFontHeight( g, FONT ) + 2;
+			}
+			
 			g.setColor( NOTIFICATION_BG_COLOR );
-			g.fillRect( x, y, w, h );
+			g.fillRect( x, y, _width, _height );
 			g.setColor( NOTIFICATION_FG_COLOR );
-			g.drawString( content, x, y ) ;
+			for( String c: _content ) {
+				g.drawString( c, x, y );
+				y += _lineHeight;
+			}
 		}
+		
+		public int getHeight(){ return _height; }
+		public Notification getNotification(){ return _notification; }
 	}
 }
